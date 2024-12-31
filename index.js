727cb80788cbc8fa35ceaf62702d80a51d6ee050
MohamedHesham45 <mohmedadm733@gmail.com>
2024-12-31 22:49:07 +0200

    FEAT:remove images

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 3001;
@@ -25,6 +26,10 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 
+// Add body-parser middleware
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
 app.post('/upload-images', upload.array('images', 10), (req, res) => {
   if (!req.files || req.files.length === 0) {
     // Create a new file if no files are uploaded
@@ -39,6 +44,31 @@ app.post('/upload-images', upload.array('images', 10), (req, res) => {
   
   const filePaths = req.files.map(file => file.path); 
   res.status(200).json({ message: 'Images uploaded successfully', files: filePaths });
+});
+
+app.delete('/remove-images', (req, res) => {
+  console.log('Received request to delete images', req.body);
+  const { filenames } = req.body; // Expecting an array of filenames to delete
+
+  if (!filenames || !Array.isArray(filenames) || filenames.length === 0) {
+    return res.status(400).send('No filenames provided.');
+  }
+
+  const deletePromises = filenames.map(fullPath => {
+    const filename = path.basename(fullPath); // Extract the filename from the full path
+    const filePath = path.join(uploadsDir, filename);
+    return fs.promises.unlink(filePath).catch(err => {
+      console.error(`Error deleting file ${filename}:`, err);
+      return null; // Return null for failed deletions
+    });
+  });
+
+  Promise.all(deletePromises).then(results => {
+    const deletedFiles = results.filter(result => result !== null);
+    res.status(200).json({ message: 'Files deleted successfully'});
+  }).catch(err => {
+    res.status(500).send('Error deleting files.');
+  });
 });
 
 app.listen(port, () => {
